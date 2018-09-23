a371c2f7e20ac3c3762685f69ce57dbb7983c911
TeamDman <TeamDman9201@gmail.com>
2018-09-23 02:32:52 -0400

    Added who command, fixed logic that gets reactors, updated help
    Ensured mostly valid parameters

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ Sends the `start` embed from the config to the specified channel and adds the gi
 
 #### /lotto end #channel message_id count
 Selects `count` members that reacted to the specified channel message and sends the `end` embed to the channel.
+
+#### /lotto who #channel message_id
+Displays the number of users who have reacted to the given message, and randomly displays 10 of those users.
+
+
+Still WIP, but I'm calling it a night.
+Code so far: https://github.com/TeamDman/Pick-Bot
--- main.py
+++ main.py
@@ -31,6 +31,14 @@ def has_perms(member, channel):
     return any([role.id in config["perms"]["roles that allow commands"] for role in member.roles])
 
 
+async def get_reactors(reactions):
+    rtn = []
+    for reaction in reactions:
+        while len(rtn) < reaction.count:
+            rtn += await client.get_reaction_users(reaction, after=None if len(rtn) == 0 else rtn[-1])
+    return set([member for member in rtn if not member.bot])
+
+
 async def notify_winner(member):
     await client.send_message(member, config["winner direct message"])
 
@@ -50,7 +58,7 @@ class MyClient(discord.Client):
             return
         args = message.content[len(prefix):].split()
         if args[0] == "help":
-            await client.send_message(message.channel, "zoop")
+            await client.send_message(message.channel, "https://github.com/TeamDman/Pick-Bot")
         elif args[0] == "eval":
             result = eval(str.join(" ", args[1:]))
             em = discord.Embed()
@@ -62,6 +70,9 @@ class MyClient(discord.Client):
             em.description = str(result)
             await client.send_message(message.channel, embed=em)
         elif args[0] == "react":
+            if not len(args) == 4:
+                await client.send_message(message.channel, "Invalid arguments. Required:\nreact channel message emote")
+                return
             react = args[3]
             messageid = args[2]
             channelid = get_id_from_str(args[1])
@@ -69,6 +80,14 @@ class MyClient(discord.Client):
             message = await client.get_message(channel, messageid)
             await client.add_reaction(message, react)
         elif args[0] == "start":
+            if not len(args) == 3:
+                await client.send_message(message.channel, "Invalid arguments. Required:\nstart channel emote")
+                return
+            try:
+                await client.add_reaction(message, args[2])
+            except discord.errors.HTTPException:
+                await client.send_message(message.channel, "The bot can't use that emote. Try again.")
+                return
             em = discord.Embed()
             em.title = config["embed start"]["title"]
             em.colour = config["embed start"]["colour"]
@@ -82,21 +101,18 @@ class MyClient(discord.Client):
             await client.add_reaction(sent, args[2])
             await client.send_message(message.channel, "Sent message id: {}".format(sent.id))
         elif args[0] == "end":
+            if not len(args) == 4:
+                await client.send_message(message.channel,
+                                          "Invalid arguments. Required:\nend channel message winner_count")
+                return
             channelid = get_id_from_str(args[1])
             channel = client.get_channel(channelid)
             sent = await client.get_message(channel, args[2])
-            winners = []
-            for reaction in sent.reactions:
-                winners += [
-                    member for member in
-                    await client.get_reaction_users(reaction, limit=reaction.count)
-                    if not member.bot
-                ]
-            winners = set(winners)
+            winners = await get_reactors(sent.reactions)
             if len(winners) < int(args[3]) or int(args[3]) < 0:
                 await client.send_message(message.channel, "Pick count invalid. (0-{})".format(len(winners)))
                 return
-            winners = random.sample(set(winners), int(args[3]))
+            winners = random.sample(winners, int(args[3]))
 
             for winner in winners:
                 await notify_winner(winner)
@@ -109,6 +125,20 @@ class MyClient(discord.Client):
                 em.add_field(name=field["name"], value=field["value"])
 
             await client.send_message(sent.channel, embed=em)
+        elif args[0] == "who":
+            if not len(args) == 3:
+                await client.send_message(message.channel, "Invalid arguments. Required:\nwho channel message")
+                return
+            channelid = get_id_from_str(args[1])
+            channel = client.get_channel(channelid)
+            sent = await client.get_message(channel, args[2])
+            winners = await get_reactors(sent.reactions)
+            em = discord.Embed()
+            em.title = "React WhoIs"
+            em.add_field(name="Unique", value="{} users".format(len(winners)))
+            em.add_field(name="Random 10", value="\n".join(
+                [user.name for user in random.sample(winners, len(winners) if len(winners) < 10 else 10)]))
+            await client.send_message(message.channel, embed=em)
 
     async def on_reaction_add(self, reaction, user):
         if not is_member_valid(user):
